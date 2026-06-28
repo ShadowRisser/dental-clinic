dd041739c72849701344047f46c7f721016b59d2
Saim Mirza <saim@shadowriser.com>
2026-06-28 10:31:43 +0500

    feat: deploy static site

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,17 @@
-     1|import type { NextConfig } from "next";
-     2|
-     3|const nextConfig: NextConfig = {
-     4|  output: "export",
-     5|  basePath: '/dental-clinic',
-     6|  assetPrefix: '/dental-clinic/',
-     7|  images: {
-     8|    unoptimized: true,
-     9|  },
-    10|  /* config options here */
-    11|  typescript: {
-    12|    ignoreBuildErrors: true,
-    13|  },
-    14|  reactStrictMode: false,
-    15|};
-    16|
-    17|export default nextConfig;
-    18|
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  output: "export",
+  basePath: '/dental-clinic',
+  assetPrefix: '/dental-clinic/',
+  images: {
+    unoptimized: true,
+  },
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  reactStrictMode: false,
+};
+
+export default nextConfig;
